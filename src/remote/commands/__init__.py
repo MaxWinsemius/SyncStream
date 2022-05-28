85aa6dafa938813d00b8121ddd4b230d3da74c40
Max Winsemius <maxwinsemius@me.com>
2022-05-28 22:42:30 +0200

    Added skeleton for commands

--- src/remote/commands/__init__.py
+++ src/remote/commands/__init__.py
@@ -5,8 +5,17 @@
 
 from . import udpserver
 from . import animations
+from . import ssd
+
+# Required methods for commands are:
+# def add_parser_subcommand(parser):
+#     pass
+#
+# def exec(args):
+#     pass
 
 all = [
     udpserver,
-    animations
+    animations,
+    ssd
 ]
